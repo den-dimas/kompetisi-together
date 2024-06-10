0a42c90af9adb1ed235b257adc2ad58a02374d39
Den <dimass.drm@gmail.com>
2024-06-10 20:15:03 +0700

    Add detailed users route

--- server/src/routes/participant.routes.js
+++ server/src/routes/participant.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 import * as participantController from "../controllers/participant.controller.js";
+import { requireAuth, requireParticipant } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
@@ -8,5 +9,10 @@ const router = express.Router();
 router.post("/login", participantController.login);
 router.post("/logout", participantController.logout);
 router.post("/register", participantController.register);
+
+router.get("/:id", requireAuth, participantController.getParticipantById);
+router.put("/:id", requireParticipant, participantController.changeParticipant);
+
+router.get("/:id/kompetisi", requireParticipant, participantController.getParticipantKompetisi)
 
 export default router;
--- server/src/routes/penyelenggara.routes.js
+++ server/src/routes/penyelenggara.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 import * as penyelenggaraController from "../controllers/penyelenggara.controller.js";
+import { requireAuth, requirePenyelenggara } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
@@ -8,5 +9,11 @@ const router = express.Router();
 router.post("/login", penyelenggaraController.login);
 router.post("/logout", penyelenggaraController.logout);
 router.post("/register", penyelenggaraController.register);
+
+router.get("/:id", requireAuth, penyelenggaraController.getPenyelenggaraById);
+router.put("/:id", requirePenyelenggara, penyelenggaraController.changePenyelenggara);
+
+
+router.get("/:id/kompetisi", requirePenyelenggara, penyelenggaraController.getPenyelenggaraKompetisi)
 
 export default router;
